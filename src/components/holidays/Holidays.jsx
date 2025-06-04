c2939cf45f96f716d7f9561d074a05d77aed9d1a
Alexandr Maspanov <alexandrmaspanov@gmail.com>
2025-06-04 21:32:57 +0300

    fix: correct holidays component

--- src/components/holidays/Holidays.jsx
+++ src/components/holidays/Holidays.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import styles from './Holidays.module.css';
 import Loader from '../UI/loader/Loader';
+import { API_KEYS } from '../../config';
+import TranslateButton from '../translatebutton/TranslateButton';
 
 const Holidays = () => {
+    const HOLIDAYS_API_KEY = API_KEYS.holidays;
     const [holidays, setHolidays] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
@@ -16,7 +19,7 @@ const Holidays = () => {
             const year = today.getFullYear();
             const todayStr = today.toISOString().slice(0, 10);
 
-            const url = `https://date.nager.at/api/v3/PublicHolidays/${year}/BY`;
+            const url = `https://calendarific.com/api/v2/holidays?&api_key=${HOLIDAYS_API_KEY}&country=BY&year=${year}`;
             const res = await fetch(url);
 
             if (!res.ok) {
@@ -24,8 +27,8 @@ const Holidays = () => {
             }
 
             const data = await res.json();
-            const todayHolidays = data.find(item => item.date === todayStr);
-            setHolidays(todayHolidays?.localName);
+            const todayHolidays = data?.response?.holidays?.find(item => item?.date?.iso === todayStr);
+            setHolidays(todayHolidays?.name);
         } catch (error) {
             console.error('Ошибка при получении праздников ', error);
             setError(error.message || 'Неизвестная ошибка');
@@ -48,14 +51,17 @@ const Holidays = () => {
             ) : (
                 <>
                     {holidays ? (
-                    <p className={styles.holidays}>
-                        {holidays.join(', ')}
-                    </p>
+                        <>
+                            <p className={styles.holidays}>
+                                {holidays}
+                            </p>
+                            <TranslateButton text={`${holidays}`} />
+                        </>
                     ) : (
-                    <p>Официальных праздников сегодня нет</p>
+                        <p>Официальных праздников сегодня нет</p>
                     )}
                     <div className={styles.source}>
-                        Данные предоставлены сервисом: <a href='https://date.nager.at/PublicHoliday/Belarus' target='_blank' rel='noopener noreferrer'>Nager.Date</a>
+                        Данные предоставлены сервисом: <a href='https://calendarific.com/holidays/2025/BY' target='_blank' rel='noopener noreferrer'>Calendarific</a>
                     </div>
                 </>
             )}
--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export const API_KEYS = {
     weather: '20c17fa0527dde3a8f1efbd032e803fd',
+    holidays: '72oXS4pewNd8fjTbHZbO84WABfC38dN7',
     space: '2RnngmjKxxTUoN49Z6fhqROnwcau9xN5xS50NqRg',
 };
